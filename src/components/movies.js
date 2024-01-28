e06a9279c2a0126b4d5e4f9bf321e0a34fa5234b
Daniel Ji <daniel.ji0923@gmail.com>
2024-01-27 20:57:22 -0800

    boilerplate done

---
+++ src/components/movies.js
@@ -0,0 +1,11 @@
+const Movies = (props) => {
+
+    return (
+        <div className="helo">
+            This is ComponentName
+        </div>
+    );
+}
+
+// export the component
+export default Movies
